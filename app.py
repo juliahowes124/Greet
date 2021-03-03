4da4e70188d00ec6246f1ea69c7872bd0eeecd20
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-03-03 11:27:07 -0800

    added basic routes

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def return_welcome():
+    return "welcome"
+
+
+@app.route('/welcome/home')
+def welcome_home():
+    return "welcome home"
+
+
+@app.route('/welcome/back')
+def welcome_back():
+    return "welcome back"
